b28631cb433f863d9a608c9341974ff27961c326
Cside <cside.story@gmail.com>
2018-03-08 06:18:31 +0900

    screen で別タブにしてるとき発動しなくなるので、とりあえず...

--- conf.d/done.fish
+++ conf.d/done.fish
@@ -22,34 +22,18 @@
 
 set -g __done_version 1.4.1
 
-function __done_get_window_id
-	if type -q lsappinfo
-		lsappinfo info -only bundleID (lsappinfo front) | cut -d '"' -f4
-	else if type -q xprop
-	and test $DISPLAY
-		xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2
-	end
-end
-
-
 # verify that the system has graphical capabilites before initializing
 if test -z "$SSH_CLIENT"  # not over ssh
-and test -n __done_get_window_id  # is able to get window id
 
 	set -g __done_initial_window_id ''
 	set -q __done_min_cmd_duration; or set -g __done_min_cmd_duration 5000
 	set -q __done_exclude; or set -g __done_exclude 'git (?!push|pull)'
-
-	function __done_started --on-event fish_preexec
-		set __done_initial_window_id (__done_get_window_id)
-	end
 
 	function __done_ended --on-event fish_prompt
 		set -l exit_status $status
 
 		if test $CMD_DURATION
 		and test $CMD_DURATION -gt $__done_min_cmd_duration # longer than notify_duration
-		and test $__done_initial_window_id != (__done_get_window_id)  # terminal or window not in foreground
 		and not string match -qr $__done_exclude $history[1] # don't notify on git commands which might wait external editor
 
 			# Store duration of last command
